efe42e1849452e3ae9173eacc0858732238886c0
seongjin77 <kssj9056@gmail.com>
2022-08-04 19:12:46 +0900

    코딩테스트

--- 인프런 강의/섹션3/5. 문자열 압축.js
+++ 인프런 강의/섹션3/5. 문자열 압축.js
@@ -23,3 +23,19 @@ function solution (str){
 
 let str ='KKHSSSSSSSE'
 console.log(solution(str));
+
+
+function solution(s){
+    let answer="";
+    let cnt=1;
+    s=s+" "; // s[i+1]에 공백값을 넣어주어 에러가 나지 않게 한다.
+    for(let i=0; i<s.length-1; i++){
+        if(s[i]===s[i+1]) cnt++;
+        else{
+            answer+=s[i];
+            if(cnt>1) answer+=String(cnt);
+            cnt=1;
+        }
+    }
+    return answer;
+}
