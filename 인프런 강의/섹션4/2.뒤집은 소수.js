43ead7dcbb133e98ef8c0fb5bd62519d5f57dbcb
seongjin77 <kssj9056@gmail.com>
2022-08-08 21:08:27 +0900

    소수찾기

---
+++ 인프런 강의/섹션4/2.뒤집은 소수.js
@@ -0,0 +1,14 @@
+function solution(arr){
+ let answer = [];
+
+ const primeNumber = () => {
+
+ }
+
+for(let x of arr){
+    let res=Number(x.toString().split('').reverse().join(''));
+    
+}
+
+ console.log(answer);
+};
