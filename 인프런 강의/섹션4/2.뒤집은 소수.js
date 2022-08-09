699db7b35ca6b43ee8d63e86625ddd9c00cff862
seongjin77 <kssj9056@gmail.com>
2022-08-09 21:30:24 +0900

    소수 찾기

--- 인프런 강의/섹션4/2.뒤집은 소수.js
+++ 인프런 강의/섹션4/2.뒤집은 소수.js
@@ -1,14 +1,26 @@
 function solution(arr){
  let answer = [];
 
- const primeNumber = () => {
-
- }
+ const eratosthenes = (n) => {
+    const arr = Array(n+1).fill(true)
+    arr[0] = false
+    arr[1] = false
+    for (let i = 2; i ** 2 < n+1; ++i) {
+        if (!arr[i]) continue 
+        for (let j = 2 * i; j < n+1; j += i) arr[j] = false
+    }
+    return arr[n];
+} 
 
 for(let x of arr){
     let res=Number(x.toString().split('').reverse().join(''));
-    
+    if(eratosthenes(res)){
+        answer.push(res)
+    }
 }
 
  console.log(answer);
 };
+
+let num = [32,55,62,20,250,370,200,30,100]
+solution(num);
