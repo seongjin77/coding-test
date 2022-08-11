fe454e2375428e9621678c7015e66b79a5a7e2b6
seongjin77 <kssj9056@gmail.com>
2022-08-11 20:16:39 +0900

    다른 문제풀이

--- 인프런 강의/fdfsfs.js
+++ 인프런 강의/fdfsfs.js
@@ -1,12 +1,16 @@
-const eratosthenes = (n) => {
-    const arr = Array(n+1).fill(true)
-    arr[0] = false
-    arr[1] = false
-    for (let i = 2; i ** 2 < n+1; ++i) {
-        if (!arr[i]) continue 
-        for (let j = 2 * i; j < n+1; j += i) arr[j] = false
-    }
-    return arr
-}
-console.log(eratosthenes(3))
+// const eratosthenes = (n) => {
+//     const arr = Array(n+1).fill(true)
+//     arr[0] = false
+//     arr[1] = false
+//     for (let i = 2; i ** 2 < n+1; ++i) {
+//         if (!arr[i]) continue 
+//         for (let j = 2 * i; j < n+1; j += i) arr[j] = false
+//     }
+//     return arr
+// }
+// console.log(eratosthenes(3))
 
+function solution(){
+    var a =1;
+}
+console.log(a);
--- 인프런 강의/섹션4/1. 자릿수의 합.js
+++ 인프런 강의/섹션4/1. 자릿수의 합.js
@@ -1,30 +1,30 @@
 function solution(arr) {
   let answer;
-  // 내림차순으로 정리한다.
+  // 내림차순으로 정리한다. (각자리수의 합이 동일할 경우 더 큰 수를 얻기 위해)
   // 각각의 배열값을 문자열로 만들어서, 각 자리수를 쪼개 더한다.
   // 데이터에서 가장 큰 숫자를 찾는다. 해당 인덱스 번호에 값을 내림차순 한 원본 데이터에서 찾는다.
 
   arr.sort((a, b) => b - a);
-  let 문자저장;
-  let 숫자저장 = 0;
-  let 큰수 = 0;
+  let str;
+  let num = 0;
+  let maxNum = 0;
   let 비교배열 = [];
   for (let i = 0; i < arr.length; i++) {
-    문자저장 = String(arr[i]); // arr는 문자열로 바꿈
+    str = String(arr[i]); // arr는 문자열로 바꿈
 
-    for (let j = 0; j < 문자저장.length; j++) {
-      숫자저장 += parseInt(문자저장[j]); // 숫자타입을 변수에 담아서 더해주려면 변수에 숫자타입이 있어야한다. 이유는???
+    for (let j = 0; j < str.length; j++) {
+      num += parseInt(str[j]); 
     }// 각자리수 합 for문
-    비교배열.push(숫자저장) // 원본 배열과 위치는 똑같으니, 자리수 합 배열의 제일 큰 수의 인덱스를 알아 원본 배열의 원래값을 찾기 위해서.
+    비교배열.push(num) // 원본 배열과 위치는 똑같으니, 자리수 합 배열의 제일 큰 수의 인덱스를 알아 원본 배열의 원래값을 찾기 위해서.
     // [128, 460, 603, 40, 521, 137, 123]
     // [  9, 8, 10, 11, 11, 6,  4]
-    if (큰수 < 숫자저장) {
-      큰수 = 숫자저장;
+    if (maxNum < num) {
+      maxNum = num;
     }
-    숫자저장 = 0; // 각 자리수 합의 초기화
+    num = 0; // 각 자리수 합의 초기화
 }// 원본 배열 for문
 
-  answer = arr[비교배열.indexOf(큰수)];
+  answer = arr[비교배열.indexOf(maxNum)];
 
   console.log(answer)
 }
