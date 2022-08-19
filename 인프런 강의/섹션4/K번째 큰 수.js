af6f2d38295e78d01d8fe13724a7307a2db5de45
seongjin77 <kssj9056@gmail.com>
2022-08-19 18:55:56 +0900

    코딩테스트

--- 인프런 강의/섹션4/K번째 큰 수.js
+++ 인프런 강의/섹션4/K번째 큰 수.js
@@ -1,20 +1,32 @@
-function solution(arr, selectNumber){
-    let answer;
-    const results = [];
-  if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+function solution(arr,selectNumber){
+
+const sum = [];
+
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  
+  if (selectNumber === 1) return arr.map((el) => [el]); 
+  // n개중에서 1개 선택할 때(nC1), 바로 모든 배열의 원소 return
 
   arr.forEach((fixed, index, origin) => {
-    const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
-    const combinations = solution(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
-    const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
-    results.push(...attached); // 배열 spread syntax 로 모두다 push
+    const rest = origin.slice(index + 1); 
+    // 해당하는 fixed를 제외한 나머지 뒤
+    const combinations = getCombinations(rest, selectNumber - 1); 
+    // 나머지에 대해서 조합을 구한다.
+    const attached = combinations.map((el) => [fixed, ...el]); 
+    //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+    results.push(...attached); 
+    // 배열 spread syntax 로 모두다 push
   });
 
-  console.log(results) ; // 결과 담긴 results return
-
+   return results; //조합 결과 확인용
 }
 
+let 조합 = getCombinations(arr, selectNumber);
+console.log(조합)
 
+}
 let arr = [13,15,34,23,45,65,33,11,26,42];
 let k = 3;
 solution(arr,k)
+
