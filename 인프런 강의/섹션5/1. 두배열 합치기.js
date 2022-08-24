6a4fbcc86e779320942ca7bf78007d559f2b1700
seongjin77 <kssj9056@gmail.com>
2022-08-24 21:32:16 +0900

    코딩테스트

--- 인프런 강의/섹션5/1. 두배열 합치기.js
+++ 인프런 강의/섹션5/1. 두배열 합치기.js
@@ -1,17 +1,11 @@
-function solution(arr1, arr2){
-    let answer=[];
-    let n=arr1.length;
-    let m=arr2.length;
-    let p1=p2=0;
-    while(p1<n && p2<m){
-        if(arr1[p1]<=arr2[p2]) answer.push(arr1[p1++]);
-        else answer.push(arr2[p2++]);
-    }
-    while(p1<n) answer.push(arr1[p1++]);
-    while(p2<m) answer.push(arr2[p2++]); 
-    return answer;
+function solution (left, right){
+    let sum = [...left, ...right];
+    sum.sort((a,b)=>a-b);
+
+    return sum
 }
 
-let a=[1, 3, 5];
-let b=[2, 3, 6, 7, 9];
-console.log(solution(a, b));
+let left = [1,3,5];
+let right =[2,3,6,7,9];
+
+console.log(solution(left, right));
